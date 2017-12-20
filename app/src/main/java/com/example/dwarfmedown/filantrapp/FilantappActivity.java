188621c25f7e08a96c12167d917c9d9e09d595d1
miguelsott <miguelsott@gmail.com>
2017-12-20 05:05:00 -0200

    Commit Inicial - Adicionada dependencia da ButterKnife

---
+++ app/src/main/java/com/example/dwarfmedown/filantrapp/FilantappActivity.java
@@ -0,0 +1,13 @@
+package com.example.dwarfmedown.filantrapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FilantappActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filantapp);
+    }
+}
